48cfaf18fe16f85385c8f0ee6b884c69bfac1d2c
Michał Jopa <micha.jopa@gmail.com>
2023-09-28 14:10:34 +0200

    share and comments

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm
+from django.core.mail import send_mail
 
 
 def post_list(request):
@@ -18,8 +20,55 @@ def post_list(request):
 
 
 def post_detail(request, year, month, day, post):
+
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    #     # List of similar posts
+    # post_tags_ids = post.tags.values_list('id', flat=True)
+    # similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+    #     .exclude(id=post.id)
+    # similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+    #                     .order_by('-same_tags', '-publish')[:4]
+
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form,
+                   # 'similar_posts': similar_posts
+                   })
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f"{cd['name']} zachęca przeczytać {post.title}"
+            message = f"Przeczytaj {post.title} na stronie {post_url}\n\n" \
+                      f"{cd['name']}\'komentarz dodany przez: {cd['comments']}"
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 class PostListView(ListView):
